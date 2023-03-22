b64ed33785d76a516ccf91afbef8f0bdd320f38c
ilaria <ilaria@ilariafiorini.it>
2023-03-22 17:45:40 +0100

    aggoirnato function.php

--- themes/twentytwentythree/functions.php
+++ themes/twentytwentythree/functions.php
@@ -1,6 +1,8 @@
 <?php
 function custom_style(){
     wp_enqueue_style( "wpcf7-custom", get_stylesheet_directory_uri(). "/assets/css/wpcf7.css",array(), false );
-    wp_enqueue_script( "custom", get_stylesheet_directory_uri(). "/assets/js/script.js",array(), null );
+    if (is_page(7)){
+        wp_enqueue_script( "custom", get_stylesheet_directory_uri(). "/assets/js/script.js",array(), null );
+    }
 }
 add_action('wp_enqueue_scripts', 'custom_style');
