ec9179a1fbfce467b19c49dae5acb0a6e9d8ff31
ilaria <ilaria@ilariafiorini.it>
2023-03-22 17:40:02 +0100

    aggiunto una fake dir con dentro im files creati oggi

---
+++ themes/twentytwentythree/functions.php
@@ -0,0 +1,6 @@
+<?php
+function custom_style(){
+    wp_enqueue_style( "wpcf7-custom", get_stylesheet_directory_uri(). "/assets/css/wpcf7.css",array(), false );
+    wp_enqueue_script( "custom", get_stylesheet_directory_uri(). "/assets/js/script.js",array(), null );
+}
+add_action('wp_enqueue_scripts', 'custom_style');
